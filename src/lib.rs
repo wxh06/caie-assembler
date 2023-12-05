790f3c8fd8c06bcc45603ed93f216f862e5105f2
Xinhe Wang <wangxinhe06@gmail.com>
2023-12-05 18:33:28 +0800

    Index register

--- src/lib.rs
+++ src/lib.rs
@@ -3,23 +3,43 @@ use std::collections::HashMap;
 type Address = usize;
 type Number = i32;
 
+enum Register {
+    ACC,
+    IX,
+}
+
 enum Operand {
     Number(Number),
     Address(Address),
 }
 
 enum Instruction {
-    Load(Operand),
+    Load(Register, Operand),
+    Move(Register),
     Store(Address),
-    Add(Operand),
-    Subtract(Operand),
+    Add(Register, Operand),
+    Subtract(Register, Operand),
     OUT,
+}
+
+struct Registers {
+    accumulator: Number,
+    index_register: Number,
+}
+
+impl Registers {
+    pub fn get_register_mut(&mut self, register: &Register) -> &mut Number {
+        match register {
+            Register::ACC => &mut self.accumulator,
+            Register::IX => &mut self.index_register,
+        }
+    }
 }
 
 struct Execution {
     instructions: Vec<Instruction>,
     memory: HashMap<Address, Number>,
-    accumulator: Number,
+    registers: Registers,
 }
 
 impl Execution {
@@ -27,7 +47,17 @@ impl Execution {
         Execution {
             instructions,
             memory: Default::default(),
-            accumulator: 0,
+            registers: Registers {
+                accumulator: 0,
+                index_register: 0,
+            },
+        }
+    }
+
+    pub fn get_operand_value(&self, operand: &Operand) -> Number {
+        *match operand {
+            Operand::Number(number) => number,
+            Operand::Address(address) => self.memory.get(&address).expect("invalid address"),
         }
     }
 
@@ -36,29 +66,22 @@ impl Execution {
         while i < self.instructions.len() {
             let instruction = &self.instructions[i];
             match instruction {
-                Instruction::Load(operand) => {
-                    self.accumulator = *match operand {
-                        Operand::Number(number) => number,
-                        Operand::Address(address) => {
-                            self.memory.get(&address).expect("invalid address")
-                        }
-                    }
+                Instruction::Load(register, operand) => {
+                    *self.registers.get_register_mut(register) = self.get_operand_value(operand)
+                }
+                Instruction::Move(register) => {
+                    *self.registers.get_register_mut(register) = self.registers.accumulator
                 }
                 Instruction::Store(address) => {
-                    self.memory.insert(*address, self.accumulator);
+                    self.memory.insert(*address, self.registers.accumulator);
                 }
-                Instruction::Add(operand) | Instruction::Subtract(operand) => {
-                    let number = match operand {
-                        Operand::Number(number) => number,
-                        Operand::Address(address) => self.memory.get(&address).expect(""),
-                    };
-                    match instruction {
-                        Instruction::Add(_) => self.accumulator += number,
-                        Instruction::Subtract(_) => self.accumulator -= number,
-                        _ => {}
-                    }
+                Instruction::Add(register, operand) => {
+                    *self.registers.get_register_mut(register) += self.get_operand_value(operand)
                 }
-                Instruction::OUT => println!("{}", self.accumulator),
+                Instruction::Subtract(register, operand) => {
+                    *self.registers.get_register_mut(register) -= self.get_operand_value(operand)
+                }
+                Instruction::OUT => println!("{}", self.registers.accumulator),
             }
             i += 1;
         }
@@ -70,42 +93,45 @@ mod tests {
     use super::*;
 
     #[test]
-    fn ldm() {
-        let mut execution = Execution::new(vec![Instruction::Load(Operand::Number(1))]);
+    fn ldm_and_mov() {
+        let mut execution = Execution::new(vec![
+            Instruction::Load(Register::ACC, Operand::Number(1)),
+            Instruction::Move(Register::IX),
+        ]);
         execution.execute();
-        assert_eq!(execution.accumulator, 1);
+        assert_eq!(execution.registers.accumulator, 1);
+        assert_eq!(execution.registers.index_register, 1);
     }
 
     #[test]
-    fn ldd() {
-        let mut execution = Execution::new(vec![Instruction::Load(Operand::Address(2))]);
-        execution.memory.insert(2, 1);
+    fn ldd_and_sto() {
+        let mut execution = Execution::new(vec![
+            Instruction::Load(Register::IX, Operand::Address(0)),
+            Instruction::Store(1),
+        ]);
+        execution.memory.insert(0, 1);
         execution.execute();
-        assert_eq!(execution.accumulator, 1);
-    }
-
-    #[test]
-    fn sto() {
-        let mut execution = Execution::new(vec![Instruction::Store(0)]);
-        execution.execute();
-        assert_eq!(execution.memory.get(&0), Some(&0));
+        assert_eq!(execution.registers.index_register, 1);
+        assert_eq!(execution.memory.get(&1), Some(&0));
     }
 
     #[test]
     fn add_and_sub_number() {
         let mut execution = Execution::new(vec![
-            Instruction::Add(Operand::Number(2)),
-            Instruction::Subtract(Operand::Number(1)),
+            Instruction::Add(Register::IX, Operand::Number(1)),
+            Instruction::Subtract(Register::ACC, Operand::Number(-1)),
         ]);
         execution.execute();
-        assert_eq!(execution.accumulator, 1);
+        assert_eq!(execution.registers.accumulator, 1);
+        assert_eq!(execution.registers.index_register, 1);
     }
 
     #[test]
     fn add_address() {
-        let mut execution = Execution::new(vec![Instruction::Add(Operand::Address(0))]);
+        let mut execution =
+            Execution::new(vec![Instruction::Add(Register::ACC, Operand::Address(0))]);
         execution.memory.insert(0, 1);
         execution.execute();
-        assert_eq!(execution.accumulator, 1);
+        assert_eq!(execution.registers.accumulator, 1);
     }
 }
