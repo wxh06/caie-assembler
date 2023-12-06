8137de2ea402ed8076745be5fa1dc5339458b461
Xinhe Wang <wangxinhe06@gmail.com>
2023-12-06 20:43:47 +0800

    rust-wasm

--- assembler/src/lib.rs
+++ assembler/src/lib.rs
@@ -1,3 +1,16 @@
 mod instruction;
 mod operand;
 mod register;
+mod utils;
+
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern "C" {
+    fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet() {
+    alert("Hello, assembler!");
+}
