7aa74bee4b22d98250292e7c9cf44a32b71f2df2
Xinhe Wang <wangxinhe06@gmail.com>
2023-12-08 00:17:12 +0800

    Remove

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,14 @@
-pub mod instruction;
-pub mod operand;
-pub mod parser;
-pub mod register;
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
--- src/operand.rs
+++
@@ -1,7 +0,0 @@
-pub type Address = usize;
-pub type Number = i32;
-
-pub enum Operand {
-    Number(Number),
-    Address(Address),
-}
