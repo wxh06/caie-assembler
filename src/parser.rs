ac2ca25904908d58f653e99c66217f48463980e5
Xinhe Wang <wangxinhe06@gmail.com>
2023-12-09 21:21:12 +0800

    Return `Result` instead of `panic!`

--- src/parser.rs
+++ src/parser.rs
@@ -5,7 +5,9 @@ use crate::utils::set_panic_hook;
 use crate::Assembly;
 
 trait OperandParser {
-    fn parse(string: &str) -> Self;
+    fn parse(string: &str) -> Result<Self, String>
+    where
+        Self: Sized;
 }
 
 enum Operand {
@@ -14,95 +16,113 @@ enum Operand {
 }
 
 impl OperandParser for Number {
-    fn parse(string: &str) -> Self {
+    fn parse(string: &str) -> Result<Self, String> {
         let prefix = &string[..1];
         let number = &string[1..];
         match prefix {
-            "#" => Self::from_str_radix(number, 10).expect("Invalid denary number"),
-            "B" => Self::from_str_radix(number, 2).expect("Invalid binary number"),
-            "&" => Self::from_str_radix(number, 16).expect("Invalid hexadecimal number"),
-            _ => panic!("Invalid number"),
+            "#" => Self::from_str_radix(number, 10)
+                .map_err(|_| format!("Invalid denary number `{}`", number)),
+            "B" => Self::from_str_radix(number, 2)
+                .map_err(|_| format!("Invalid binary number `{}`", number)),
+            "&" => Self::from_str_radix(number, 16)
+                .map_err(|_| format!("Invalid hexadecimal number `{}`", number)),
+            _ => Err(format!("Invalid number `{}`", string)),
         }
     }
 }
 
 impl OperandParser for Address {
-    fn parse(string: &str) -> Self {
+    fn parse(string: &str) -> Result<Self, String> {
         let absolute = string.parse();
         match absolute {
-            Ok(address) => Self::Absolute(address),
-            Err(_) => Self::Symbolic(String::from(string)),
+            Ok(address) => Ok(Self::Absolute(address)),
+            Err(_) => Ok(Self::Symbolic(String::from(string))),
         }
     }
 }
 
 impl OperandParser for Operand {
-    fn parse(string: &str) -> Self {
-        let number = string.parse();
-        match number {
-            Ok(number) => Self::Number(number),
-            Err(_) => Self::Address(Address::parse(string)),
+    fn parse(string: &str) -> Result<Self, String> {
+        match Number::parse(string) {
+            Ok(number) => Ok(Self::Number(number)),
+            Err(_) => match Address::parse(string) {
+                Ok(address) => Ok(Self::Address(address)),
+                Err(_) => Err(format!("Invalid operand `{}`", string)),
+            },
         }
     }
 }
 
 impl OperandParser for Register {
-    fn parse(string: &str) -> Self {
+    fn parse(string: &str) -> Result<Self, String> {
         match string {
-            "ACC" => Self::ACC,
-            "IX" => Self::IX,
-            _ => panic!("Invalid register"),
+            "ACC" => Ok(Self::ACC),
+            "IX" => Ok(Self::IX),
+            _ => Err(format!("Invalid register `{}`", string)),
         }
     }
 }
 
 impl Operation {
-    fn parse<'a>(opcode: &str, operands: &mut impl Iterator<Item = &'a str>) -> Self {
-        let mut next_operand = || {
-            operands
-                .next()
-                .expect(&format!("Too few operands to {}", opcode))
-        };
+    fn parse<'a>(
+        opcode: &str,
+        operands: &mut impl Iterator<Item = &'a str>,
+    ) -> Result<Self, String> {
+        macro_rules! operand {
+            ($ty:ident) => {
+                $ty::parse(
+                    operands
+                        .next()
+                        .ok_or_else(|| format!("Too few operands to `{}`", opcode))?,
+                )?
+            };
+        }
+        macro_rules! operation {
+            ($op:ident, $ty:ident) => {
+                Self::$op(operand!($ty))
+            };
+        }
+
         let operation = match opcode {
-            "LDM" => Self::LoadImmediate(Number::parse(next_operand())),
-            "LDD" => Self::LoadDirect(Address::parse(next_operand())),
-            "LDI" => Self::LoadIndirect(Address::parse(next_operand())),
-            "LDX" => Self::LoadIndexed(Address::parse(next_operand())),
-            "LDR" => Self::LoadRegister(Number::parse(next_operand())),
-            "MOV" => Self::Move(Register::parse(next_operand())),
-            "STO" => Self::Store(Address::parse(next_operand())),
-            "ADD" => match Operand::parse(next_operand()) {
+            "LDM" => operation!(LoadImmediate, Number),
+            "LDD" => operation!(LoadDirect, Address),
+            "LDI" => operation!(LoadIndirect, Address),
+            "LDX" => operation!(LoadIndexed, Address),
+            "LDR" => operation!(LoadRegister, Number),
+            "MOV" => operation!(Move, Register),
+            "STO" => operation!(Store, Address),
+            "ADD" => match operand!(Operand) {
                 Operand::Address(address) => Self::AddDirect(address),
                 Operand::Number(number) => Self::AddImmediate(number),
             },
-            "SUB" => match Operand::parse(next_operand()) {
+            "SUB" => match operand!(Operand) {
                 Operand::Address(address) => Self::SubtractDirect(address),
                 Operand::Number(number) => Self::SubtractImmediate(number),
             },
-            "INC" => Self::Increase(Register::parse(next_operand())),
-            "DEC" => Self::Decrease(Register::parse(next_operand())),
-            "JMP" => Self::Jump(Address::parse(next_operand())),
-            "CMP" => match Operand::parse(next_operand()) {
+            "INC" => operation!(Increase, Register),
+            "DEC" => operation!(Decrease, Register),
+            "JMP" => operation!(Jump, Address),
+            "CMP" => match operand!(Operand) {
                 Operand::Address(address) => Self::CompareDirect(address),
                 Operand::Number(number) => Self::CompareImmediate(number),
             },
-            "CMI" => Self::CompareIndirect(Address::parse(next_operand())),
-            "JPE" => Self::JumpEqual(Address::parse(next_operand())),
-            "JPN" => Self::JumpNotEqual(Address::parse(next_operand())),
+            "CMI" => operation!(CompareIndirect, Address),
+            "JPE" => operation!(JumpEqual, Address),
+            "JPN" => operation!(JumpNotEqual, Address),
             "IN" => Self::Input,
             "OUT" => Self::Output,
             "END" => Self::End,
-            _ => panic!("Unknown opcode `{}`", opcode),
+            _ => return Err(format!("Unknown opcode `{}`", opcode)),
         };
         if operands.next().is_some() {
-            panic!("Too many operands to {}", opcode)
+            return Err(format!("Too many operands to `{}`", opcode));
         }
-        operation
+        Ok(operation)
     }
 }
 
 impl Instruction {
-    fn parse(string: &str) -> Self {
+    fn parse(string: &str) -> Result<Self, String> {
         set_panic_hook();
         let mut instruction: Self = Default::default();
         let mut split = string.split_ascii_whitespace();
@@ -113,26 +133,26 @@ impl Instruction {
                     .push(String::from(&segment[0..(segment.len() - 1)]));
                 continue;
             }
-            instruction.operation = Some(Operation::parse(segment, &mut split));
+            instruction.operation = Some(Operation::parse(segment, &mut split)?);
             break;
         }
-        instruction
+        Ok(instruction)
     }
 }
 
 #[wasm_bindgen]
 impl Assembly {
     #[wasm_bindgen(constructor)]
-    pub fn new(source: &str, offset: AbsoluteAddress) -> Self {
-        Self {
-            instructions: Self::parse(&mut source.split_terminator("\n")),
+    pub fn new(source: &str, offset: AbsoluteAddress) -> Result<Assembly, String> {
+        Ok(Self {
+            instructions: Self::parse(&mut source.split_terminator('\n'))?,
             offset,
-        }
+        })
     }
-    fn parse<'a>(instructions: &mut impl Iterator<Item = &'a str>) -> Vec<Instruction> {
-        instructions
-            .map(|instruction| Instruction::parse(instruction))
-            .collect()
+    fn parse<'a>(
+        instructions: &mut impl Iterator<Item = &'a str>,
+    ) -> Result<Vec<Instruction>, String> {
+        instructions.map(Instruction::parse).collect()
     }
 }
 
@@ -144,22 +164,34 @@ mod tests {
     fn parse_ldm() {
         assert_eq!(
             Instruction::parse("A: B: LDM &FF"),
-            Instruction {
+            Ok(Instruction {
                 labels: vec![String::from("A"), String::from("B")],
                 operation: Some(Operation::LoadImmediate(255)),
-            }
+            })
         );
     }
 
     #[test]
-    #[should_panic(expected = "Too many operands to END")]
-    fn parse_end() {
-        Instruction::parse("END #0");
+    fn parse_ldr() {
+        assert_eq!(
+            Instruction::parse("LDR B2"),
+            Err(String::from("Invalid binary number `2`"))
+        );
     }
 
     #[test]
-    #[should_panic(expected = "Unknown opcode `UNKNOWN`")]
+    fn parse_end() {
+        assert_eq!(
+            Instruction::parse("END #0"),
+            Err(String::from("Too many operands to `END`"))
+        );
+    }
+
+    #[test]
     fn parse_unknown() {
-        Instruction::parse("UNKNOWN");
+        assert_eq!(
+            Instruction::parse("UNKNOWN"),
+            Err(String::from("Unknown opcode `UNKNOWN`"))
+        );
     }
 }
