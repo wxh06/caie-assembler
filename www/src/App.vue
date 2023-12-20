ee89271e1f1b1b4b3a18b9bc306b7b35fc741a94
Xinhe Wang <wangxinhe06@gmail.com>
2023-12-20 12:40:22 +0800

    TraceTable

--- www/src/App.vue
+++ www/src/App.vue
@@ -1,9 +1,29 @@
 <script setup lang="ts">
 import AssemblyEditor from "./components/AssemblyEditor.vue";
+import { Assembler, Location } from "@caie-assembler/pkg";
+import TraceTable from "./components/TraceTable.vue";
+
+const steps = Assembler.from_memory([
+  new Location(200, "", "LDD", "365"),
+  new Location(201, "", "CMP", "366"),
+  new Location(202, "", "JPE", "209"),
+  new Location(203, "", "INC", "ACC"),
+  new Location(204, "", "STO", "365"),
+  new Location(205, "", "MOV", "IX"),
+  new Location(206, "", "LDX", "365"),
+  new Location(207, "", "OUT", ""),
+  new Location(208, "", "JMP", "200"),
+  new Location(209, "", "END", ""),
+  new Location(365, "", "", "1"),
+  new Location(366, "", "", "3"),
+  new Location(367, "", "", "65"),
+  new Location(368, "", "", "66"),
+]).execute(200);
 </script>
 
 <template>
   <main>
     <AssemblyEditor />
+    <TraceTable :steps="steps" />
   </main>
 </template>
---
+++ www/src/components/TraceTable.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import { Step } from "@caie-assembler/pkg";
+
+defineProps<{ steps: Step[] }>();
+</script>
+
+<template>
+  <table>
+    <thead>
+      <tr>
+        <th>PC</th>
+        <th>ACC</th>
+        <th>IX</th>
+        <th>OUT</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="(step, i) in steps" :key="i">
+        <td>{{ step.pc }}</td>
+        <td>{{ step.acc }}</td>
+        <td>{{ step.ix }}</td>
+        <td>{{ step.out }}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<style>
+table {
+  border-collapse: collapse;
+}
+
+th,
+td {
+  border: 1px solid black;
+}
+</style>
